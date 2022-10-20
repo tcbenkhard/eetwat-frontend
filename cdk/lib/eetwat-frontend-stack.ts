e0e15ec5b524c4ecf14a169c7d23d35629ca1e5e
Timo Benkhard <tcbenkhard@gmail.com>
2022-10-20 14:47:12 +0200

    pipeline

--- cdk/lib/eetwat-frontend-stack.ts
+++ cdk/lib/eetwat-frontend-stack.ts
@@ -5,7 +5,7 @@ import * as deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 
 interface EetwatFrontendStackProps extends cdk.StackProps {
@@ -18,16 +18,14 @@ export class EetwatFrontendStack extends cdk.Stack {
         super(scope, id, props);
 
         const hostnames = {
-            tst: `${props.serviceName}.tst.benkhard.com`,
+            tst: `${props.serviceName}-tst.benkhard.com`,
             prd: `${props.serviceName}.benkhard.com`
         }
 
         const WEB_APP_DOMAIN = hostnames[props.environment];
 
-        const zone = route53.HostedZone.fromLookup(this, "Zone", {
-            domainName: "com.benkhard",
-            privateZone: false
-        });
+        const hostedZoneId = ssm.StringParameter.valueForStringParameter(this, '/com/benkhard/public-hosted-zone-id');
+        const zone = route53.HostedZone.fromHostedZoneId(this, 'Zone', hostedZoneId);
 
         const siteBucket = new s3.Bucket(this, "SiteBucket", {
             bucketName: WEB_APP_DOMAIN,
@@ -36,11 +34,8 @@ export class EetwatFrontendStack extends cdk.Stack {
             removalPolicy: cdk.RemovalPolicy.DESTROY
         })
 
-        const siteCertificate = new acm.DnsValidatedCertificate(this, "SiteCertificate", {
-            domainName: WEB_APP_DOMAIN,
-            hostedZone: zone,
-            region: "us-east-1"  //standard for acm certs
-        });
+        const certificateArn = ssm.StringParameter.valueForStringParameter(this, '/com/benkhard/wildcard-certificate-cloudfront');
+        const certificate = acm.Certificate.fromCertificateArn(this, 'Certificate', certificateArn);
 
         const siteDistribution = new cloudfront.CloudFrontWebDistribution(this, "SiteDistribution", {
             originConfigs: [{
@@ -52,16 +47,16 @@ export class EetwatFrontendStack extends cdk.Stack {
                     isDefaultBehavior: true
                 }]
             }],
-            viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(siteCertificate, {
+            viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(certificate, {
                 aliases: [WEB_APP_DOMAIN],
             })
         });
 
-        new route53.ARecord(this, "SiteRecord", {
-            recordName: WEB_APP_DOMAIN,
-            target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(siteDistribution)),
-            zone
-        });
+        // new route53.ARecord(this, "SiteRecord", {
+        //     recordName: WEB_APP_DOMAIN,
+        //     target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(siteDistribution)),
+        //     zone
+        // });
 
         new deploy.BucketDeployment(this, "Deployment", {
             sources: [deploy.Source.asset("../build")],
